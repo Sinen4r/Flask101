9098bbd7e28ba051019819a640740e8e51bf94f3
Sinen4r <153398440+Sinen4r@users.noreply.github.com>
2024-11-13 11:11:35 +0100

    app updeate

--- apiwithFlask/app1.py
+++ apiwithFlask/app1.py
@@ -1,10 +1,16 @@
-from resources.courseitems import *
-from resources.specialization import *
-from flask import *
-
-app=Flask(__name__)
-
-app.config["API_VERSION"]=True
-app.config[""]=True
-app.config[""]="/"
-app.config[""]="/"
+from flask import Flask
+from flask_smorest import Api
+from flask import Flask, request, abort
+from resources.courseitems import blp as Course_item_Blueprint
+from resources.specialization import blp as Specialization_Blueprint
+app = Flask(__name__)
+app.config["PROPAGATE_EXCEPTIONS"]=True
+app.config["API_TITLE"]="TBS REST API"
+app.config["API_VERSION"]="RELEASE 1"
+app.config["OPENAPI_VERSION"]="3.0.3"
+app.config["OPENAPI_URL_PREFIX"]="/"
+app.config["OPENAPI_SWAGGER_UI_PATH"]="/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist"
+api=Api(app)
+api.register_blueprint(Course_item_Blueprint)
+api.register_blueprint(Specialization_Blueprint)
